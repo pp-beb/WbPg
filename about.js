2160ecd2832fcf47832083ab2879b9a4e25f2457
Prithak koirala <prithakkoirala44@gmail.com>
2025-01-18 15:54:02 +0545

    wyawya

---
+++ about.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="projects.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>About</h1>
+    <h2 class="HomeIcon" id="HomeIcon"><a href="index.html">&#8962;</a></h2>
+
+    <script src="projects.js"></script>
+</body>
+</html>
