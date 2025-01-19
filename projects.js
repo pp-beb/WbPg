07bc176400f405c40ffd6b8ccd1748c0aa00d025
Prithak koirala <prithakkoirala44@gmail.com>
2025-01-19 11:32:39 +0545

    wyaywa

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
-window.showTieSheet = () => window.location.href = "Tie.html";
-window.showLeague = () => window.location.href = "Final.cpp";
-window.showMusic = () => window.location.href = "music.html";
-window.showClock = () => window.location.href = "clock.html";
-window.showHover = () => window.location.href = "hoverEffects.html";
-window.showConverter = () => window.location.href = "currency.html";
+window.showTieSheet = () => window.open("Tie.html", "_blank");
+window.showLeague = () => window.open("Final.cpp", "_blank");
+window.showMusic = () => window.open("music.html", "_blank");
+window.showClock = () => window.open("clock.html", "_blank");
+window.showHover = () => window.open("hoverEffects.html", "_blank");
+window.showConverter = () => window.open("currency.html", "_blank");
