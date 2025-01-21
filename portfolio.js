ad21dfcf38d9720857b0187bfcdde97ba3d1f64c
Prithak koirala <prithakkoirala44@gmail.com>
2025-01-21 11:26:26 +0545

    wyawya

--- portfolio.js
+++ portfolio.js
@@ -2,3 +2,6 @@ window.gotoCover = () => window.location.href = "index.html";
 function gotoContent(){
     window.location.href = "index.html";
 }
+
+window.showCV = () => window.open("CV.pdf","_blank");
+window.showCCT = () => window.open("CCT.pdf","_blank");
