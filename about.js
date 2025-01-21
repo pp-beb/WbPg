6ceb1285b6e174eb6f73c70947015c1f7839a9c1
Prithak koirala <prithakkoirala44@gmail.com>
2025-01-21 10:05:00 +0545

    f

--- about.js
+++ about.js
@@ -1,4 +1,4 @@
-window.gotoCover = () => window.location.href = "index.html";
+window.gotoCover = () =>  window.open("index.html","_self");
 function gotoContent(){
     window.location.href = "index.html";
 }
