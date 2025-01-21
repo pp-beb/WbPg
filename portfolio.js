89c6ee8a8cf0cd158c697f6e3dbcead48e042fba
Prithak koirala <prithakkoirala44@gmail.com>
2025-01-21 17:53:28 +0545

    wakka

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,5 @@ function gotoContent(){
 
 window.showCV = () => window.open("CV.pdf","_blank");
 window.showCCT = () => window.open("CCT.pdf","_blank");
+window.github = () => window.open("https://github.com/pp-beb","_blank");
+window.linkedin = () => window.open("https://www.linkedin.com/in/prithak-koirala-2253802b1/?originalSubdomain=np","_blank");
